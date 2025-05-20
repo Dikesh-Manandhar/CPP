94b74cb192599016f2d8fa3bf9d92e94fc56aa0d
Dikesh Manandhar <dikeshmanandhar15@gmail.com>
2025-05-20 11:48:33 +0000

    commit ok

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -6,6 +6,18 @@
 
 int main(){
     
-    std::cout << std::endl << std::endl;
+    int a[4];
+    float tem[] = {19.5, 26.8};
+
+    a[0] = 1;
+    a[1] = 2;
+    a[3] = 4;
+    a[5] = 6;
+
+
+    std::cout << "a[0] = " << a[0]<< std::endl << std::endl;
+    std::cout << "a[1] = " << a[1]<< std::endl << std::endl;
+    std::cout << "Temp 1 = " << tem[0]<< std::endl << std::endl;
+    std::cout << "Temp 2 = " << tem[1]<< std::endl << std::endl;
     return (0);
 }
